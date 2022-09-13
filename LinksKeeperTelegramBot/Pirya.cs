24f2fb104e75c7c1820028de8488e17f84ba5885
vladpurype@gmail.com <vladpurype@gmail.com>
2022-09-13 20:04:40 +0300

    No_MUM

---
+++ LinksKeeperTelegramBot/Pirya.cs
@@ -0,0 +1,9 @@
+namespace LinksKeeperTelegramBot;
+
+public class Pirya
+{
+    public string Name { get; set; }
+    public string Len { get; set; }
+    public string Wid { get; set; }
+    public string Volume { get; set; }
+}
