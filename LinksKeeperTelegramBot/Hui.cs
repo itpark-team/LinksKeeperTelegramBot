685e2ceeef290a3e53180268d157e67488488815
itpark <a.m.smyslov@yandex.ru>
2022-09-13 20:02:41 +0300

    add important class

---
+++ LinksKeeperTelegramBot/Hui.cs
@@ -0,0 +1,9 @@
+namespace LinksKeeperTelegramBot;
+
+public class Hui
+{
+    public string Name { get; set; }
+    public int Len { get; set; }
+    public int Wid { get; set; }
+    public int Volume { get; set; }
+}
