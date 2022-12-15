ecc01f265a8b0501b2b6da23c8f27a0b97e5ec20
itpark <a.m.smyslov@yandex.ru>
2022-12-15 21:19:32 +0300

    change console readkey to exitEvent

--- LinksKeeperTelegramBot/Program.cs
+++ LinksKeeperTelegramBot/Program.cs
@@ -1,11 +1,19 @@
 ﻿using LinksKeeperTelegramBot.BotInitializer;
 
+ManualResetEvent exitEvent = new ManualResetEvent(false);
+
+Console.CancelKeyPress += (sender, eventArgs) => {
+    eventArgs.Cancel = true;
+    exitEvent.Set();
+};
+
 Bot bot = new Bot();
 bot.Start();
 
 Console.WriteLine($"Bot @{bot.GetBotName()} started");
-Console.WriteLine("Press enter for stop");
-Console.ReadKey();
+Console.WriteLine("Press Ctrl+C for stop");
+
+exitEvent.WaitOne();
 
 bot.Stop();
 
